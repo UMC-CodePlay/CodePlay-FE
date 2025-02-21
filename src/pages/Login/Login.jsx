7f8d8ee497405d84f1a76e5ba5dcc807a88e6a9e
Jeesung Yun <jeesungyun@yonsei.ac.kr>
2025-02-21 10:56:54 +0900

    1056

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -101,6 +101,7 @@ const Login = () => {
           />
           <InputField
             type="password"
+            allign='center'
             placeholder="비밀번호를 입력하세요."
             value={password}
             onChange={(e) => setPassword(e.target.value)}
